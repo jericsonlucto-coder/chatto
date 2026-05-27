a63cd277f6aadb0eefd01f8629455ae1dce42672
jericsonlucto-coder <jericson.lucto@straive.com>
2026-05-27 14:24:53 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ interface Message {
   userId: string;
 }
 
-// Simple ID generator
+// Simple ID generator - no external dependencies
 const generateId = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  return Date.now().toString(36) + Math.random().toString(36).substring(2, 9);
 };
 
 export default function Home() {
